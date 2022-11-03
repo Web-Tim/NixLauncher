0c0a8cc73dadbb3fb1f63aecdb59ccbb8d678d96
Web-Tim <teichmanntim@outlook.de>
2022-11-03 09:02:44 +0100

    c

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,11 @@ module eu.nix.nixlauncher {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.google.gson;
+    requires gson;
     requires openauth;
     requires java.desktop;
     requires java.net.http;
+    requires java.sql;
 
     opens eu.nix.nixlauncher to javafx.fxml;
     exports eu.nix.nixlauncher;
